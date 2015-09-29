6648230977590cad478b8bda47b35d9d2d4056c4
Ondiz Zarraga <Ondiz@users.noreply.github.com>
2015-09-29 14:39:35 +0200

    Update receptance4.m

--- Receptance/receptance4.m
+++ Receptance/receptance4.m
@@ -21,13 +21,13 @@ fprintf('PREDICTION OF KC USING RECEPTANCE\n')
 m1 = 1;
 m2 = 1;
 
-M = [m1 0 0 0; 0 0 m1 0; 0 0 m2 0; 0 0 0 m2]; 
+M = [m1 0 0 0; 0 m1 0 0; 0 0 m2 0; 0 0 0 m2]; 
 
 k1 = 2;
 k2 = 1;
 kx = 1;
 
-kc = 3;
+kc = 5;
 
 K = [kx 0 0 0; 0 k1+kc 0 -kc; 0 0 kx 0; 0 -kc 0 k2+kc]; 
 
@@ -43,11 +43,26 @@ h_coupled = H(2,2);
 
 % Subsystem 1:
 
-% 
-% syms kc
-% 
-% kc = solve('ha*(hb*kc+1)/(1+kc*(hb+ha))-h_coupled',kc); 
-% 
-% kc = simplify(eval(kc));
-% 
-% fprintf('\nkc = %.2f\n', eval(kc))
+Msubs1 = [m1 0; 0 m1];
+Ksubs1 = [kx 0; 0 k1];
+
+Hsubs1 = inv(s^2*Msubs1 + Ksubs1);
+
+ha = Hsubs1(2,2);
+
+% Subsystem 2:
+
+Msubs2 = [m2 0; 0 m2];
+Ksubs2 = [kx 0; 0 k2];
+
+Hsubs2 = inv(s^2*Msubs2 + Ksubs2);
+
+hb = Hsubs2(2,2);
+
+syms kc
+
+kc = solve('ha*(hb*kc+1)/(kc*(ha+hb)+1)-h_coupled',kc); % Esta fórmula es universal
+
+kc = simplify(eval(kc));
+fprintf('\nkc = %.2f\n', eval(kc))
+
